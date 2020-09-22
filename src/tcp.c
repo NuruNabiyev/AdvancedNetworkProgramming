17d2d163d3c99ec4ef1862eef22c5e4a01d6e49d
Nuru <nurik1998nabiyev@gmail.com>
2020-09-22 11:02:00 +0200

    Get tcp fields in correct byte order

--- src/tcp.c
+++ src/tcp.c
@@ -4,30 +4,12 @@
 #include "ip.h"
 #include "utilities.h" // need bcause there is do_tcp_csum
 
-void dump_hex(void *something, int len){
-    printf("DUMPING_HEX: [");
-    for(int i = 0; i < len; i++)
-    {
-        printf("%02x ", ((unsigned char*)something)[i]);
-    }
-    printf("]\n");
-}
-
-void print_tcp(struct tcp_hdr *tcp) {
-    printf("TCP_DUMP: "
-           "src_port: %hu, dest_port %hu, "
-           "\n\n",
-           htons(tcp->src_port), htons(tcp->dest_port)
-           // todo print seq, ack
-           );
-}
-
 void tcp_rx(struct subuff *sub) {
     struct iphdr *ih = IP_HDR_FROM_SUB(sub);
     struct tcp_hdr *tcp = (struct tcp_hdr *) (ih->data);
 
     dump_hex(tcp, 40);
-    print_tcp(tcp);
+    debug_tcp(tcp);
 }
 
 void tcp_tx(struct subuff *sub) {
--- src/tcp.h
+++ src/tcp.h
@@ -11,8 +11,9 @@ struct tcp_hdr {
     uint16_t dest_port;
     uint32_t seq_num;
     uint32_t ack_num;
-    uint8_t data_offset: 4;    // also indicated header length by *4
-    uint16_t res: 12;
+    //uint8_t data_offset: 4;    // also indicated header length by *4
+    //uint16_t res: 12;
+    uint16_t dataoff_res;
     uint16_t window;
     uint16_t csum;
     uint16_t up;
@@ -22,5 +23,16 @@ struct tcp_hdr {
 
 void tcp_rx(struct subuff *sub);
 void tcp_tx(struct subuff *sub);
+
+inline void debug_tcp(struct tcp_hdr *tcp) {
+    printf("TCP_DUMP: "
+           "src_port %hu, dest_port %hu, "
+           "seq %u, ack %u, "
+           "data offset and reserved %hx, "
+           "window %u, checksum %hx\n\n",
+           htons(tcp->src_port), htons(tcp->dest_port),
+           ntohl(tcp->seq_num), tcp->ack_num,   // todo ack should be converted too
+           htons(tcp->dataoff_res), htons(tcp->window), htons(tcp->csum));
+}
 
 #endif //ANPNETSTACK_TCP_H
