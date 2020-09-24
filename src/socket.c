9a5df06815b3dd65dbb28d55ecf1640a36442c0f
Detjon Shahu <sdetion@gmail.com>
2020-09-24 11:01:27 +0200

    FD increment from last socket done.

--- src/socket.c
+++ src/socket.c
@@ -5,9 +5,18 @@
 #include "systems_headers.h"
 #include "socket.h"
 
-struct sock_info *init_sock() {
+struct sock_info *init_sock(struct list_head *fd_cache) {
+    struct list_head *item;
+    struct sock_info *entry;
+    int size_counter = 0;
+    list_for_each(item, fd_cache){
+        entry = list_entry(item, struct sock_info, list);
+        size_counter++;
+    }
+    
     struct sock_info *sock_ptr = calloc(1, sizeof(struct sock_info));
-    sock_ptr->fd = get_random_number(); // random file descriptor in range of 1B-2B
+    if(size_counter > 0){ sock_ptr->fd = entry->fd + 1; } 
+    else{ sock_ptr->fd = get_random_number(); }
     sock_ptr->state = SOCK_CLOSED; // closed in the beginning
     return sock_ptr;
 }
